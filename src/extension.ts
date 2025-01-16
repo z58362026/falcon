589d6df9e035887ca7dfe250a87d63e9d9b33201
mh.yao <minghao.yao@gientech.com>
2025-01-16 15:51:48 +0800

    feat:0.0.2

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand("falcon.helloWorld", () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage("Hello World VS Code!");
+        vscode.window.showInformationMessage("Hello World VS Code111111111!");
     });
 
     context.subscriptions.push(disposable);
